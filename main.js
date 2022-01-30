523fe80c3fb84eedd574edfc0c0f31c86598c11c
Arjun Yuvaraj <arjunycoding@gmail.com>
2022-01-30 15:23:39 -0500

    fixing bugs

--- main.js
+++ main.js
@@ -2,32 +2,50 @@ let createTodo = $("#createTodo")
 let newTodo = $("#newTodo")
 let heading = $("#heading")
 let color = "alert"
+let error = $(".error")
 let el = 0
 let prevColor = 0
 
-$(".editOptins").hide()
+// When the ""New Todo butn is clicked
 newTodo.on("click",function(){
     heading.html("New Todo")
+    // Hide and show
     $(".newTodo").show()
     $("#home").hide()
     newTodo.hide()
+    $(".editOptins").hide()
+    $("#createTodo").show()
 })
-
+// When a color is clicked
 $(".color").on("click",function(){
     color += $(this).attr('class').split(' ')[2].slice(3)
 })
 
+// When the create todo btn is clicked
 createTodo.on("click", function(){
-    heading.html("Current Todos")
     let titleVal = $("input#title").val()
     let memoVal = $.trim($("#memo").val())
-    $(".todos").append(
-        `<div class="alert ${color} todoItem" role="alert">
+    if(titleVal == ""){
+        error.show()
+        return
+    }
+    if(memo == ""){
+        $(".todos").append(
+            `<div class="alert ${color} todoItem" role="alert">
+            <b id="todoTitle">${titleVal}</b>
+            </div>`)        
+    } else {
+        $(".todos").append(
+            `<div class="alert ${color} todoItem" role="alert">
             <b id="todoTitle">${titleVal}</b> --> ${memoVal}
-        </div>`
-    )
+            </div>`
+        )
+    } 
+    console.log(`Hi "${memoVal}"`)
+    heading.html("Current Todos")
     $("input#title").val("")
     $.trim($("#memo").val(""))
+    prevColor = color
     color = "alert"
     $(".newTodo").hide()    
     $("#home").show()
@@ -36,11 +54,9 @@ createTodo.on("click", function(){
     $(".todos").show()
     createTodo.show()
     $(".editOptins").hide()
-    // console.log($(".todoItem"))
 })
 
 $("#home").on("click", ".todoItem", function(){
-    prevColor = color
     let item = $(this).text()
     el = $(this)
     let splitted = item.replace(/\s{2,}/g, '').split('-->');
@@ -54,19 +70,21 @@ $("#home").on("click", ".todoItem", function(){
     $("#home").hide()
     $("#createTodo").hide()
     $(".editOptins").show()
-
-
+    
+    
 })
 
 $("#save").on("click", function(){
     heading.html("Current Todos")
     let titleVal = $("input#title").val()
     let memoVal = $.trim($("#memo").val())
+    console.log(color, prevColor)
     el.removeClass(prevColor);
     el.addClass(color);
     el.html(`<b id="todoTitle">${titleVal}</b> --> ${memoVal}`)
     $("input#title").val("")
     $.trim($("#memo").val(""))
+    prevColor = color
     color = "alert"
     $(".newTodo").hide()    
     $("#home").show()
